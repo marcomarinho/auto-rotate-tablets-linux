27d786f43c045b8aa7bd64770a08725df902219e
Marco Marinho <marcomarinho12@gmail.com>
2021-11-28 12:47:54 +0000

    Update rotate.sh

--- rotate.sh
+++ rotate.sh
@@ -1,3 +1,29 @@
+#!/bin/sh
+# Auto rotate screen based on device orientation
+
+GDBUS=gdbus
+XINPUT=xinput
+XRANDR=xrandr
+
+# Receives input from monitor-sensor (part of iio-sensor-proxy package)
+# Screen orientation and launcher location is set based upon accelerometer position
+# Launcher will be on the left in a landscape orientation and on the bottom in a portrait orientation
+# This script should be added to startup applications for the user
+
+# Clear sensor.log so it doesn't get too long over time
+> sensor.log
+
+# Launch monitor-sensor and store the output in a variable that can be parsed by the rest of the script
+monitor-sensor >> sensor.log 2>&1 &
+
+# Parse output or monitor sensor to get the new orientation whenever the log file is updated
+# Possibles are: normal, bottom-up, right-up, left-up
+# Light data will be ignored
+while inotifywait -e modify sensor.log; do
+# Read the last line that was added to the file and get the orientation
+CURRENTORIENTATION=$(tail -n 1 sensor.log | grep 'orientation' | grep -oE '[^ ]+$')
+
+# Functions
 get_dbus_orientation () {
     # Get the orientation from the DBus
     #
@@ -51,3 +77,143 @@ get_dbus_orientation () {
     # Return the orientation found
     echo $ORIENTATION
 }
+
+do_rotate () {
+    # Rotate screen and pointers
+    #
+    # $1: The requested mode (only "screen" gets a special treatment)
+    # $2: The new orientation
+    # $3: The screen to rotate
+    # $4-: The pointers to rotate
+
+    TRANSFORM='Coordinate Transformation Matrix'
+
+    MODE=$1
+    shift
+
+    ORIENTATION=$1
+    shift
+
+    # Rotate the screen
+    if test $MODE != screen; then
+        # Only rotate it, if we have not got the orientation from the screen
+        $XRANDR --output $1 --rotate $ORIENTATION
+    fi
+    shift
+
+    # Rotate all pointers
+    while test $# -gt 0; do
+        case $ORIENTATION in
+            normal)
+                $XINPUT set-prop $1 "$TRANSFORM" 1 0 0 0 1 0 0 0 1
+                ;;
+            inverted)
+                $XINPUT set-prop $1 "$TRANSFORM" -1 0 1 0 -1 1 0 0 1
+                ;;
+            left)
+                $XINPUT set-prop $1 "$TRANSFORM" 0 -1 1 1 0 0 0 0 1
+                ;;
+            right)
+                $XINPUT set-prop $1 "$TRANSFORM" 0 1 0 -1 0 1 0 0 1
+                ;;
+        esac
+        shift
+    done
+}
+
+MODE=auto
+# Get the display
+XDISPLAY=$($XRANDR --current --verbose | grep primary | cut --delimiter=" " -f1)
+
+# Get the tablet's orientation
+case $MODE in
+    auto)
+        ORIENTATION=$(get_dbus_orientation)
+        ret=$?
+        if test $ret != 0; then
+            echo $ORIENTATION
+            echo "(To use this script, supply the orientation normal, inverted, left or right on the command line.)"
+            exit $ret
+        fi
+        ;;
+    screen)
+        ORIENTATION=$(get_screen_orientation $XDISPLAY)
+        ret=$?
+        if test $ret != 0; then
+            echo $ORIENTATION
+            exit $ret
+        fi
+        ;;
+    next)
+        ORIENTATION=$(get_screen_orientation $XDISPLAY)
+        ret=$?
+        if test $ret != 0; then
+            echo $ORIENTATION
+            exit $ret
+        fi
+        case $ORIENTATION in
+            normal)
+                ORIENTATION=left
+                ;;
+            left)
+                ORIENTATION=inverted
+                ;;
+            inverted)
+                ORIENTATION=right
+                ;;
+            right)
+                ORIENTATION=normal
+                ;;
+            *)
+                ORIENTATION=normal
+                ;;
+        esac
+        ;;
+    previous)
+        ORIENTATION=$(get_screen_orientation $XDISPLAY)
+        ret=$?
+        if test $ret != 0; then
+            echo $ORIENTATION
+            exit $ret
+        fi
+        case $ORIENTATION in
+            normal)
+                ORIENTATION=right
+                ;;
+            left)
+                ORIENTATION=normal
+                ;;
+            inverted)
+                ORIENTATION=left
+                ;;
+            right)
+                ORIENTATION=inverted
+                ;;
+            *)
+                ORIENTATION=normal
+                ;;
+        esac
+        ;;
+    normal|inverted|left|right)
+        ORIENTATION=$MODE
+        ;;
+    *)
+        echo "Unknown command line parameter orientation $MODE"
+        exit 1
+esac
+
+# Get all pointers
+POINTERS=$($XINPUT | grep slave | grep pointer | sed -e 's/^.*id=\([[:digit:]]\+\).*$/\1/')
+
+# Set the actions to be taken for each possible orientation
+case "$CURRENTORIENTATION" in
+normal)
+do_rotate $MODE normal $XDISPLAY $POINTERS ;;
+bottom-up)
+do_rotate $MODE inverted $XDISPLAY $POINTERS ;;
+right-up)
+do_rotate $MODE right $XDISPLAY $POINTERS ;;
+left-up)
+do_rotate $MODE left $XDISPLAY $POINTERS ;;
+esac
+done
